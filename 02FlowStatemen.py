f130b422fa171a1f3819809b22971c33d8d28cb3
dacru <dacru@psb.vib-ugent.be>
2019-08-27 18:07:16 +0200

    Finished data structure

--- 02FlowStatemen.py
+++ 02FlowStatemen.py
@@ -1,4 +1,4 @@
-ph = 5.1
+ph = 51
 
 if ph > 7:
     print('basic')
@@ -10,7 +10,6 @@ else:
 print('done')
 
 
-
 mass = int(input("Enter you weight (Kg): "))
 height = int(input("ENter height: "))
 
@@ -18,8 +17,31 @@ BMI = mass / (height / 100) ** 2
 
 print(BMI)
 
+#2.1 ISBN
+# read ten digits of an ISBN-10 code (each on a separate line)
+x1 = int(input())
+x2 = int(input())
+x3 = int(input())
+x4 = int(input())
+x5 = int(input())
+x6 = int(input())
+x7 = int(input())
+x8 = int(input())
+x9 = int(input())
+x10 = int(input())
+# compute check digit
+check_digit = (
+x1 + 2 * x2 + 3 * x3 + 4 * x4 + 5 * x5 + 6 * x6 + 7 * x7 + 8 * x8 + 9 * x9
+) % 11
+# check correctness of check digit
+print("OK" if x10 == check_digit else "WRONG")
 
-#Runway
+if x10 == check_digit:
+    print("OK")
+else:
+    print("WRONG")
+
+#2.2 Runway
 # read magnetic azimuth of the runway’s heading
 angle = float(input())
 # compute first runway number
@@ -35,6 +57,38 @@ if direction2 < direction1:
     direction1, direction2 = direction2, direction1
 # output runway number in format dd/dd
 print(f'{direction1:02d}/{direction2:02d}')
+
+#2.3 Digit work
+# read two factors
+factor1 = int(input())
+factor2 = int(input())
+# determine smallest and largest factor
+smallest, largest = min(factor1, factor2), max(factor1, factor2)
+# use fingers to compute product of two factors
+if (0 <= smallest <= largest <= 5) or factor1 == 10 or factor2 == 10:
+    # method 0: product of two integers between 0 and 5
+    print(f"{factor1} x {factor2} = {factor1 * factor2}")
+elif 6 <= smallest <= largest <= 10:
+    # method 1: product of two integers between 6 and 10
+    print("{} x {} = 10 x ({} + {}) + ({} x {}) = {}".format(
+        factor1,
+        factor2,
+        factor1 - 5,
+        factor2 - 5,
+        10 - factor1,
+        10 - factor2,
+        factor1 * factor2
+    ))
+else:
+    # method 2: product of two integers between 10 and 15
+    # note: this method also works in general for other products
+    print("{0} x {1} = 100 + 10 x ({2} + {3}) + ({2} x {3}) = {4}".format(
+        factor1,
+        factor2,
+        factor1 - 10,
+        factor2 - 10,
+        factor1 * factor2
+    ))
 
 
 #During class
